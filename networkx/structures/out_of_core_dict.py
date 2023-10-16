5199c634741a99abc729303895296c148362cbc9
leogm99 <lgiampieri62@gmail.com>
2023-10-15 23:40:21 -0300

    Inherit from mutable mapping to get default python implementations

--- networkx/structures/out_of_core_dict.py
+++ networkx/structures/out_of_core_dict.py
@@ -1,11 +1,14 @@
 import tempfile
+from typing import MutableMapping
+
 import plyvel
 import pickle
 
-class OutOfCoreDict:
+
+class OutOfCoreDict(MutableMapping):
     def __init__(self) -> None:
         self._temp = tempfile.TemporaryDirectory()
-        self._inner = plyvel.DB(f'{self._temp.name}', create_if_missing=True)
+        self._inner = plyvel.DB(f"{self._temp.name}", create_if_missing=True)
         self._count = 0
 
     @staticmethod
@@ -17,37 +20,39 @@ class OutOfCoreDict:
         return pickle.loads(any_bytes)
 
     def __getitem__(self, key):
-        if not key in self:
-            raise KeyError(f'{key}')
         key_b = OutOfCoreDict.__to_bytes(key)
+        if self._inner.get(key_b) is None:
+            raise KeyError(key)
         return OutOfCoreDict.__from_bytes(self._inner.get(key_b))
 
     def __setitem__(self, key, value):
         key_b = OutOfCoreDict.__to_bytes(key)
         value_b = OutOfCoreDict.__to_bytes(value)
-        if not key in self:
+        if self._inner.get(key_b) is None:
             self._count += 1
         self._inner.put(key_b, value_b, sync=True)
 
     def __len__(self):
         return self._count
-    
-    def __contains__(self, key):
-        # TODO: refactor
-        # self._inner.__contains__ is not implemented
-        # maybe using iterators?
-        key_b = OutOfCoreDict.__to_bytes(key)
-        maybe_key = self._inner.get(key_b)
-        return maybe_key is not None
-    
+
     def __inner_iter_items(self):
         for k, v in self._inner:
             yield OutOfCoreDict.__from_bytes(k), OutOfCoreDict.__from_bytes(v)
-    
+
     def __iter__(self):
         for k, _ in self.__inner_iter_items():
             yield k
 
-    def items(self):
-        yield from self.__inner_iter_items()
+    def __del__(self):
+        self.__dealloc()
 
+    def __delitem__(self, key) -> None:
+        key_b = OutOfCoreDict.__to_bytes(key)
+        if not self._inner.get(key_b):
+            raise KeyError(key)
+        self._inner.delete(key_b)
+        self._count -= 1
+
+    def __dealloc(self):
+        self._inner.close()
+        self._temp.cleanup()
