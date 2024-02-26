a1500bdf70dd0ee7a399ef1f0fa8048376cbcef3
Fran <fvazquezf@fi.uba.ar>
2024-02-26 20:38:07 -0300

    added all test results

--- MemTesting/ScriptLazy.py
+++ MemTesting/ScriptLazy.py
@@ -4,12 +4,12 @@ import time
 def main():
     # TODO: change path
     # start_time = time.time()
-    G = nx.LazyGraph.from_edgelist_file('/home/grey/networkx/MemTesting/gplus_combined.txt')
-    # G = nx.LazyGraph.from_edgelist_file('/home/grey/networkx/MemTesting/soc-pokec-relationships.txt', '\t')
+    # G = nx.LazyGraph.from_edgelist_file('/home/grey/networkx/MemTesting/1912.edges')
+    G = nx.LazyGraph.from_edgelist_file('/home/grey/networkx/MemTesting/soc-pokec-relationships.txt', '\t')
     # G = nx.read_edgelist("", delimiter="\t")
 
     # G = nx.read_edgelist("1912.edges", delimiter=" ")
-    # degree_centrality = nx.degree_centrality(G)
+    nx.degree_centrality(G)
     # rank = dict(sorted(degree_centrality.items(), key=lambda item: item[1], reverse=True)[:5])
     # end_time = time.time()
     # print(end_time - start_time)
