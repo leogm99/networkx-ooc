47ea03e40ea86c903aa5ff185417fbd1293ae74b
joaquinhojman <hojmanjoaquin@gmail.com>
2024-04-27 17:49:26 -0300

    PrimitiveDict accept None as value

--- networkx/structures/primitive_dicts.py
+++ networkx/structures/primitive_dicts.py
@@ -42,21 +42,29 @@ class PrimitiveDict(OutOfCoreDict):
         yield from map(self.__deserialize_key, super().prefix_iter(self.__serialize_key(prefix)))
 
     def __serialize_key(self, key):
+        if key is None:
+            return b'\x00' * struct.calcsize(self._key_format)
         if self._key_format == PrimitiveType.EDGE:
             return struct.pack(self._key_format, *key)
         return struct.pack(self._key_format, key)
 
     def __serialize_value(self, value):
+        if value is None:
+            return b'\x00' * struct.calcsize(self._value_format)
         if self._value_format == PrimitiveType.EDGE:
             return struct.pack(self._value_format, *value)
         return struct.pack(self._value_format, value)
 
     def __deserialize_key(self, key):
+        if key == b'\x00' * struct.calcsize(self._key_format):
+            return None
         if self._key_format == PrimitiveType.EDGE:
             return struct.unpack(self._key_format, key)
         return struct.unpack(self._key_format, key)[0]
 
     def __deserialize_value(self, value):
+        if value == b'\x00' * struct.calcsize(self._value_format):
+            return None
         if self._value_format == PrimitiveType.EDGE:
             return struct.unpack(self._value_format, value)
         return struct.unpack(self._value_format, value)[0]
